3c0aa94911df7e7a0be417a4e7b19874ba88fc25
Marcelo Vallejos Tinta <vallejostinta@gmail.com>
2026-06-01 22:04:34 -0400

    se añade tabla de usuarios

--- config/db.js
+++ config/db.js
@@ -36,6 +36,16 @@ async function connectDB() {
     )
   `);
 
+  await db.exec(`
+    CREATE TABLE IF NOT EXISTS users (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      nombre TEXT NOT NULL,
+      email TEXT NOT NULL UNIQUE,
+      password TEXT NOT NULL,
+      createdAt TEXT NOT NULL
+    )
+  `);
+
   console.log("Conectado correctamente a SQLite");
 }
 
